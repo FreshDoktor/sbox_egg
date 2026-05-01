eb6bc03c59ddd6c206f4d070a1e51d9e22ea6330
FreshDoktor <freshdoktor@freshgaming.eu>
2026-05-01 15:15:35 +0200

    add missing dep in install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,6 +5,7 @@
 #  als root. Zielverzeichnis: /mnt/server
 # ============================================================
 
+dpkg --add-architecture i386
 apt-get update
 apt-get install -y --no-install-recommends curl lib32gcc-s1 lib32stdc++6
 
